079d87c18aa7f8222aedd9f1af6e9f33c1aff4c1
JamesGauvreau <james.longfrenchname@gmail.com>
2023-11-30 21:51:53 -0800

    initial pseudocode

--- PopDemCalculator.js
+++ PopDemCalculator.js
@@ -1,8 +1,19 @@
 // PSEUDO
 // The initial version will be hard-coded. 
 // 
-// basePopulation: sum of (living) cohorts
+// populationWixes: sum of (living) cohorts
+// populationMuggles: [determine later]
+    // Possibly unnecessary
+// popGrowthMuggles: [determine later]
+// muggleBirths: [determine later]
+    // Initially we'll compile in twenty-year groups, but later we'll track this by decades (cohorts will also be decade-long later).
+// mugglebornBirths: [determine later; perhaps 0.01] * muggleBirths
+// cohortMortality: 5% 
+    // Later, we'll adjust this by cohort. 
+
 // Four Cohorts: cohort_N, cohort_0, cohort_20, and cohort_old. 
+
+
     // Each generation, or turn: 
         // 1) cohort_N is given a value based on cohort_20 * cohortFertility. 
         // 2) all cohorts are reduced by cohort_mortality.
@@ -12,3 +23,10 @@
         // 6) cohort_N = 0.
         // Output
         // 
+
+// Work on the below after we've got that output going. 
+    // Within each cohort, track "Native Wixes" and "Muggle-borns." 
+        // Each segment is adjusted by generation as described below, then compiled into their respective cohorts.
+    // marriageRate: [determine later]
+    // marriedWixes: cohort * 
+    // nativeWixes
